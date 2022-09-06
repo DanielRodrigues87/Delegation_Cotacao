c0878bc15db08d1d34193c2817f10e6524a55130
DanielRodrigues87 <67766681+DanielRodrigues87@users.noreply.github.com>
2022-09-06 15:16:25 -0300

    Primeiro commit

---
+++ Delegation_cotação/Account.java
@@ -0,0 +1,17 @@
+public class Account {
+
+    private Double value;
+    public Double getBalance() {
+        return value;
+    }
+
+    public Double getBalance(Conversion conversion) {
+        return conversion.convert(value);
+    } 
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    
+}
---
+++ Delegation_cotação/Conversion.java
@@ -0,0 +1,4 @@
+public interface Conversion {
+    
+    Double convert(Double value);
+}
